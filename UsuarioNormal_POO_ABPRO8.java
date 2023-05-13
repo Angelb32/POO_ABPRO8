6cea4630a45f87cc3162e236dab3da90bd40c72c
AngelB <angelbritoc.32@gmail.com>
2023-05-13 00:34:39 -0400

    6

--- UsuarioNormal_POO_ABPRO8.java
+++ UsuarioNormal_POO_ABPRO8.java
@@ -1,17 +1,52 @@
 package Tareas;
 
-	public class UsuarioNormal_POO_ABPRO8 extends Usuario_POO_ABPRO8 {
-
+public class UsuarioNormal_POO_ABPRO8 extends Usuario_POO_ABPRO8 {
+    private String nombreUsuario;
+    private String contraseña;
     private int nivel;
 
-    public UsuarioNormal_POO_ABPRO8(String nombre, String username, String password, int nivel) {
-        super(nombre, username, password);
+    public UsuarioNormal_POO_ABPRO8(String nombre, String nombreUsuario, String contraseña, int nivel) {
+        super(nombre);
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
         this.nivel = nivel;
     }
 
-    public void aumentarNivel() {
-        this.nivel++;
+    public String getNombreUsuario() {
+        return nombreUsuario;
     }
 
-    // otros métodos específicos de usuario normal
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    @Override
+    public void mostrarInformacion() {
+        super.mostrarInformacion();
+        System.out.println("Nombre de usuario: " + nombreUsuario);
+        System.out.println("Contraseña: " + contraseña);
+        System.out.println("Nivel: " + nivel);
+    }
+
+    @Override
+    public void analizarUsuario() {
+        super.analizarUsuario();
+        System.out.println("Análisis de usuario normal");
+    }
 }

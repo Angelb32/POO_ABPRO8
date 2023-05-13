dcccc89d14f6e159ee50a7496ac915569ec6a875
AngelB <angelbritoc.32@gmail.com>
2023-05-12 20:31:42 -0400

    4

--- Administrativo_POO_ABPRO8.java
+++ Administrativo_POO_ABPRO8.java
@@ -1,15 +1,23 @@
 package Tareas;
 
-	public class Administrativo_POO_ABPRO8 extends Usuario_POO_ABPRO8 implements Asesoria_POO_ABPRO8 {
+	public class Administrativo_POO_ABPRO8 extends Usuario_POO_ABPRO8 implements 	Asesoria_POO_ABPRO8 {
     private String area;
-    private String experienciaPrev;
+    private int experienciaPrev;
 
-    public Administrativo_POO_ABPRO8(String nombre, String apellido, String email, String clave, String area, String experienciaPrev) {
-        super(nombre, apellido, email, clave);
+    public Administrativo_POO_ABPRO8(String nombre, String usuario, String contraseña, String 	area, int experienciaPrev) {
+        super(nombre, usuario, contraseña);
         this.area = area;
         this.experienciaPrev = experienciaPrev;
     }
 
+    @Override
+    public void analizarUsuario() {
+        super.analizarUsuario();
+        System.out.println("Área: " + area);
+        System.out.println("Experiencia previa: " + experienciaPrev + " años");
+    }
+
+    // Getters y setters
     public String getArea() {
         return area;
     }
@@ -18,18 +26,11 @@ package Tareas;
         this.area = area;
     }
 
-    public String getExperienciaPrev() {
+    public int getExperienciaPrev() {
         return experienciaPrev;
     }
 
-    public void setExperienciaPrev(String experienciaPrev) {
+    public void setExperienciaPrev(int experienciaPrev) {
         this.experienciaPrev = experienciaPrev;
-    }
-
-    @Override
-    public void analizarUsuario() {
-        super.analizarUsuario();
-        System.out.println("Área: " + this.getArea());
-        System.out.println("Experiencia previa: " + this.getExperienciaPrev());
     }
 }

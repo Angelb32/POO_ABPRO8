bffa8fa8d8ebb08c0950c387106ccaa6302f9602
AngelB <angelbritoc.32@gmail.com>
2023-05-12 20:26:38 -0400

    4

--- Profesional_POO_ABPRO8.java
+++ Profesional_POO_ABPRO8.java
@@ -1,21 +1,13 @@
 package Tareas;
 
-	public class Profesional_POO_ABPRO8 extends Usuario_POO_ABPRO8 implements Asesoria_POO_ABPRO8 {
-
+public class Profesional_POO_ABPRO8 extends Usuario_POO_ABPRO8 implements Asesoria_POO_ABPRO8 {
     private String titulo;
     private String fechaIngreso;
 
-    public Profesional_POO_ABPRO8(String nombre, String username, String email, String titulo, String fechaIngreso) {
-        super(nombre, username, email);
+    public Profesional_POO_ABPRO8(String nombreUsuario, String contrasena, String correoElectronico, String titulo, String fechaIngreso) {
+        super(nombreUsuario, contrasena, correoElectronico);
         this.titulo = titulo;
         this.fechaIngreso = fechaIngreso;
-    }
-
-    @Override
-    public void analizarUsuario() {
-        super.analizarUsuario();
-        System.out.println("Título: " + titulo);
-        System.out.println("Fecha de ingreso: " + fechaIngreso);
     }
 
     public String getTitulo() {
@@ -32,5 +24,12 @@ package Tareas;
 
     public void setFechaIngreso(String fechaIngreso) {
         this.fechaIngreso = fechaIngreso;
+    }
+
+    @Override
+    public void analizarUsuario() {
+        super.analizarUsuario();
+        System.out.println("Título: " + titulo);
+        System.out.println("Fecha de ingreso: " + fechaIngreso);
     }
 }
